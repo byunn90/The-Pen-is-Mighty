c9c303cdd7f9283a413f2e4dcde6fe1c80431f19
byunn90 <kayhanturk90@Live.com>
2023-02-16 19:29:50 +1100

    path

--- server.js
+++ server.js
@@ -6,19 +6,17 @@ const PORT = 3001;
 // const htmlRoute = require("./routes/html");
 
 const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // this allows to excess other routes like the index filies
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  const filePath = path.join(__dirname, "./Develop/public/notes.html");
-  console.log("File path:", filePath);
-  res.sendFile(filePath);
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 // console.log(path.join(__dirname, "/public/notes.html"));
 app.listen(PORT, () =>
